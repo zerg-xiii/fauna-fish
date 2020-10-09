623285ab24ad292d5d732ba28f2bba37f003b221
zerg-xiii <63544654+zerg-xiii@users.noreply.github.com>
2020-10-09 16:36:02 +0300

    mmm

--- src/script.js
+++ src/script.js
@@ -1,52 +1,21 @@
+a0.onclick = function showD0(){ show(0) };
+a1.onclick = function showD1(){ show(1) };
+a2.onclick = function showD2(){ show(2) };
+a3.onclick = function showD3(){ show(3) };
+a4.onclick = function showD4(){ show(4) };
+a5.onclick = function showD5(){ show(5) };
+
+hideAll();
+
 function hideAll(){
-  b1.hidden = true;
-  b2.hidden = true;
-  b3.hidden = true;
-  b4.hidden = true;
-  b5.hidden = true;
-  b6.hidden = true;
-  a1.classList.remove('selected');
-  a2.classList.remove('selected');
-  a3.classList.remove('selected');
-  a4.classList.remove('selected');
-  a5.classList.remove('selected');
-  a6.classList.remove('selected');
-}
-function showB1(){
-  hideAll();
-  b1.hidden = false;
-  a1.classList.add('selected');
-}
-function showB2(){
-  hideAll();
-  b2.hidden = false;
-  a2.classList.add('selected');
-}
-function showB3(){
-  hideAll();
-  b3.hidden = false;
-  a3.classList.add('selected');
-}
-function showB4(){
-  hideAll();
-  b4.hidden = false;
-  a4.classList.add('selected');
-}
-function showB5(){
-  hideAll();
-  b5.hidden = false;
-  a5.classList.add('selected');
-}
-function showB6(){
-  hideAll();
-  b6.hidden = false;
-  a6.classList.add('selected');
+  for (var i = 0; i < document.getElementsByTagName('button').length; i++) {
+    document.getElementsByClassName('data_tab')[i].hidden = true;
+    x.querySelectorAll('button')[i].classList.remove('selected');
+  }
 }
 
-showB1();
-a1.onclick = showB1;
-a2.onclick = showB2;
-a3.onclick = showB3;
-a4.onclick = showB4;
-a5.onclick = showB5;
-a6.onclick = showB6;
+function show(i){
+  hideAll();
+  document.getElementsByClassName('data_tab')[i].hidden = false;
+  x.querySelectorAll('button')[i].classList.add('selected');
+}
